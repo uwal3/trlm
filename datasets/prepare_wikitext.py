fb542287dff415580d0d562207c6053ac9f6c02e
unwale <ndubrovin22@gmail.com>
2025-11-19 21:18:35 +0300

    (refactor): use configs in dataset preparation datasets

---
+++ datasets/prepare_wikitext.py
@@ -0,0 +1,60 @@
+import os
+
+import hydra
+import numpy as np
+from omegaconf import DictConfig
+from transformers import AutoTokenizer
+
+from datasets import load_dataset
+
+
+@hydra.main(version_base=None, config_path="../conf/data", config_name="wikitext.yaml")
+def main(cfg: DictConfig):
+    cfg = cfg._group_
+
+    output_dir = cfg.dir
+    os.makedirs(output_dir, exist_ok=True)
+
+    dataset = load_dataset(cfg.name, cfg.config, num_proc=cfg.num_workers)
+
+    splits = {
+        "train": dataset["train"],  # type: ignore
+        "val": dataset["validation"],  # type: ignore
+    }
+
+    enc = AutoTokenizer.from_pretrained(cfg.tokenizer)
+
+    def tokenize_function(examples):
+        return enc(examples["text"], add_special_tokens=False)
+
+    for split_name, ds_split in splits.items():
+        print(f"processing '{split_name}' split...")
+
+        ds_split = ds_split.filter(  # type: ignore
+            lambda x: x["text"] and not x["text"].isspace(), num_proc=cfg.num_workers
+        )
+
+        tokenized_ds = ds_split.map(
+            tokenize_function,
+            batched=True,
+            num_proc=cfg.num_workers,
+            remove_columns=["text"],
+        )
+
+        output_path = os.path.join(output_dir, f"{split_name}.bin")
+        total_tokens = 0
+
+        with open(output_path, "wb") as f:
+            for item in tokenized_ds:
+                tokens = item["input_ids"] + [enc.eos_token_id]  # type: ignore
+                f.write(np.array(tokens, dtype=np.uint16).tobytes())
+                total_tokens += len(tokens)
+
+        print(f"total tokens in '{split_name}': {total_tokens}")
+        print(f"saved to {output_path}")
+
+    print("done.")
+
+
+if __name__ == "__main__":
+    main()
